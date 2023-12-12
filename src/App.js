1a70cfbc5ee4167f2321e107cf05efe5cd3351d3
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-12-12 10:24:21 +0300

    feat: add redux provider store for App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { PagesLinks } from './components/PagesLinks';
 import { PagesRouter } from './routes/PagesRouter';
 
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js';
+
 export const App = () => (
   <BrowserRouter>
-    <header>
-      <PagesLinks />
-      <PagesRouter />
-    </header>
+    <Provider store={store}>
+
+      <header>
+        <PagesLinks />
+        <PagesRouter />
+      </header>
+      
+    </Provider>
   </BrowserRouter>
 );
