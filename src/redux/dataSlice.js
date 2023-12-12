24637b5c44beb0d3ec49779eca3e81d3a8690bc1
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2023-12-12 10:22:24 +0300

    feat: add redux files

---
+++ src/redux/dataSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState={
+  page: 1,
+  maxPage: 0,
+  episodesArr: null,
+}
+
+export const dataSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers: {
+
+    updateSaveEpisodes: (state, action) => {
+      //state.episodesArr = action.payload;
+
+      let array = action.payload;
+      let size = 8;
+      let subarray = [];
+      for (let i = 0; i < Math.ceil(array.length/size); i++) subarray[i] = array.slice((i*size), (i*size) + size);
+      state.episodesArr = subarray;
+
+
+      state.maxPage = Math.ceil(action.payload.length / 8);
+    },
+
+  },
+});
+
+export const { updateSaveEpisodes } = dataSlice.actions;
+
+export default dataSlice.reducer;
